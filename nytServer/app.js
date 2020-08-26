9f848ae02d0355e07b704ecb914c53a7d915d2a8
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-08-26 11:41:06 -0400

    added cors library to allow CORS requests

--- nytServer/app.js
+++ nytServer/app.js
@@ -1,24 +1,40 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-
+app.use(cors());
 app.use(morgan('common')); // let's see what 'common' format looks like
 
 const books = require('./books-data');
 
 app.get('/books', (req, res) => {
-  const { search = ""} = req.query;
+  const { search = "", sort } = req.query;
+
+  if (sort) {
+    if (!['title', 'rank'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of title or rank');
+    }
+  }
 
   let results = books
-      .filter(book =>
-        book
-           .title
-           .toLowerCase()
-           .includes(search.toLowerCase()));
-  
+        .filter(book =>
+            book
+              .title
+              .toLowerCase()
+              .includes(search.toLowerCase()));
+
+  if (sort) {
+    results
+      .sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
   res
-   .json(results);
+    .json(results);
 });
 
 app.listen(8000, () => {
